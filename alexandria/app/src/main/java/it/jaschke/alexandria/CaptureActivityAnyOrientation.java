1d80c16a949d079de521aa13cab3b39af15a68b9
Rafabox001 <rafabox001@gmail.com>
2015-09-10 15:12:49 -0500

    initial commit

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/CaptureActivityAnyOrientation.java
@@ -0,0 +1,7 @@
+package it.jaschke.alexandria;
+
+/**
+ * Created by Rafael on 09/09/2015.
+ */
+public class CaptureActivityAnyOrientation extends com.journeyapps.barcodescanner.CaptureActivity {
+}
